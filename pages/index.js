5d968979772c3e3931ccc0c78927461f65666bfc
Artur C. Vasconcelos <122175479+arturvasconcelos@users.noreply.github.com>
2023-05-28 00:40:02 +0000

    add home page file

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+    <div>
+        <h1>Se fiel até a morte e dar-te-ei a coroa da vida. Ap 2:10</h1>
+        <h3>O que vencer nao sofrerá a segunda morte. Ap 2:11</h3>
+    </div>
+    )
+}
+
+export default Home;
