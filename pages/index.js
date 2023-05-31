0b5af279de7fab3393e64bda680f5fef72255a8b
Artur C. Vasconcelos <122175479+arturvasconcelos@users.noreply.github.com>
2023-05-31 01:17:25 +0000

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 function Home () {
     return (
     <div>
-        <h1>Se fiel até a morte e dar-te-ei a coroa da vida. Ap 2:10</h1>
-        <h3>O que vencer nao sofrerá a segunda morte. Ap 2:11</h3>
+        <h2>Prezada Senhorita Aline Santos 👩🏻,</h2>
+        <p>É com imenso prazer e distinto apreço que me dirijo a vossa pessoa, objetivando cordialmente formalizar um convite para uma honrosa e aprazível ocasião. Gostaria de estender-lhe uma gentil solicitação para acompanharem-me ao cinema, a fim de desfrutarmos juntos da projeção do filme intitulado <strong>"Barbie"</strong>.</p>
+        <p>Em vista da sua ilustre companhia e de minha apreciação pelo seu espírito afável e distinto, considero essa ocasião propícia para compartilharmos momentos de entretenimento e descontração, envolvidos em uma trama cativante e encantadora como a presente obra cinematográfica.</p>
+        <p>Acredito sinceramente que esta experiência proporcionará um agradável deleite, ao possibilitar-nos imergir no universo de encanto e fantasia que a trama nos oferece. Seria uma honra contar com vossa presença nesse evento, contribuindo para a construção de uma memória memorável.</p>
+        <p>Sendo assim, convido respeitosamente a Senhorita Aline Santos a unir-se a mim nessa aventura cinematográfica, marcada para data e horário a serem previamente estabelecidos em acordo mútuo. Seria uma satisfação indescritível compartilhar esse momento de deleite e lazer em sua grata companhia.</p>
+        <p>Na expectativa de sua resposta e ciente de que a sua presença engrandecerá a experiência, coloco-me à disposição para esclarecer quaisquer dúvidas ou acertar detalhes pertinentes. Agradeço antecipadamente sua atenção e consideração, aguardando com ansiedade por seu gentil parecer.</p>
+        <p>Filme de homem sério 🗿🍷</p>
+        <p>Cordialmente,</p>
+        <p>Artur V.</p>
     </div>
     )
 }
